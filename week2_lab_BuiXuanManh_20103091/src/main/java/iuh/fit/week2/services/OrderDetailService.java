e3830dab0bb59597a7f9fe31ed61c90a1d7a3d2d
BuiXuanManh <91316469+BuiXuanManh@users.noreply.github.com>
2023-09-30 23:34:29 +0700

    replace project update ui and handle

--- week2_lab_BuiXuanManh_20103091/src/main/java/iuh/fit/week2/services/OrderDetailService.java
+++
@@ -1,7 +0,0 @@
-package iuh.fit.week2.services;
-
-import iuh.fit.week2.models.OrderDetail;
-
-public interface OrderDetailService {
-    boolean create(OrderDetail orderDetail);
-}
--- week2_lab_BuiXuanManh_20103091/src/main/java/iuh/fit/week2/services/ProductImageService.java
+++
@@ -1,9 +0,0 @@
-package iuh.fit.week2.services;
-
-import iuh.fit.week2.models.ProductImage;
-
-import java.util.List;
-
-public interface ProductImageService {
-    List<ProductImage> findAll();
-}
